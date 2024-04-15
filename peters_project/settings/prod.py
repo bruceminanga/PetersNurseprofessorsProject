fa5a3076f2577e37675c277d8bd76c992dece8ba
bruceminanga <bruceminanga@gmail.com>
2024-04-15 21:01:46 +0300

    settings config for the first time

--- peters_project/settings/prod.py
+++ peters_project/settings/prod.py
@@ -33,8 +33,12 @@ ALLOWED_HOSTS = ['nurseprofessors.com', 'www.nurseprofessors.com']
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',  # Verify this path
-    }
+  'default': {
+      'ENGINE': 'django.db.backends.postgresql',
+      'NAME': os.environ.get('POSTGRES_DB'),
+      'USER': os.environ.get('POSTGRES_USER'),
+      'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+      'HOST': 'db',
+      'PORT': 5432,
+  }
 }
