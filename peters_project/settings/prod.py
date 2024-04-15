92c724f2b595f53a8bc0bd94f523a0e8f0ca27e5
bruceminanga <bruceminanga@gmail.com>
2024-04-15 22:52:53 +0300

    using postgresql

--- peters_project/settings/prod.py
+++ peters_project/settings/prod.py
@@ -39,6 +39,6 @@ DATABASES = {
       'USER': os.environ.get('POSTGRES_USER'),
       'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
       'HOST': '127.0.0.1',
-      'PORT': 5432,
+      'PORT': '5432',
   }
 }
